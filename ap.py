47e664c0337557baf931fed37f73159e0a9d182a
Darth Vader <danieltarancon@gmail.com>
2023-03-10 20:16:34 +0100

    Updated

--- ap.py
+++ ap.py
@@ -1,4 +1,18 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+
+rec = Rectangle(20,10)
+
+print(str(rec), repr(rec))
 
 
 
